86e420e6bf17f702dca9dde5fbf96eab52568bbf
mrw1593 <botahamec@outlook.com>
2020-04-29 13:39:55 -0400

    Made it work on Windows

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,30 @@ mod utils;
 
 use std::string::ToString;
 
+#[cfg(windows)]
+use winapi::{
+	um::consoleapi::SetConsoleMode,
+	um::consoleapi::GetConsoleMode,
+	um::processenv::GetStdHandle,
+	um::winbase::STD_OUTPUT_HANDLE,
+	shared::minwindef::DWORD,
+	um::wincon::ENABLE_VIRTUAL_TERMINAL_PROCESSING,
+};
+
 /**
  * For all Chalks with different color types
  */
 pub trait Chalk: Sized + ToString + Default {
 	fn new() -> Self {
+		if cfg!(windows) {
+			unsafe {
+				let handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				let mut dw_mode : DWORD = 0;
+				dw_mode |= GetConsoleMode(handle, &mut dw_mode) as u32;
+				dw_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+				SetConsoleMode(handle, dw_mode);
+			}
+		}
 		Self::default()
 	}
 

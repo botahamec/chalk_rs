a70cc279a2b7bc516a943ec4ed322551c885d583
Botahamec <botahamec@outlook.com>
2020-06-01 14:24:41 -0400

    Implemented TryFrom<u8> for BasicColor

--- src/basic_chalk.rs
+++ src/basic_chalk.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 /** Foreground color using basic color */
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum BasicColor {
@@ -28,5 +30,31 @@ impl BasicColor {
 	#[inline(always)]
 	pub const fn as_background_color(self) -> u8 {
 		self as u8 + 10
+	}
+}
+
+impl TryFrom<u8> for BasicColor {
+	type Error = ();
+
+	fn try_from(value: u8) -> Result<Self, Self::Error> {
+		match value {
+			30 => Ok(BasicColor::Black),
+			31 => Ok(BasicColor::Red),
+			32 => Ok(BasicColor::Green),
+			33 => Ok(BasicColor::Yellow),
+			34 => Ok(BasicColor::Blue),
+			35 => Ok(BasicColor::Magenta),
+			36 => Ok(BasicColor::Cyan),
+			37 => Ok(BasicColor::LightGray),
+			90 => Ok(BasicColor::Gray),
+			91 => Ok(BasicColor::LightRed),
+			92 => Ok(BasicColor::LightGreen),
+			93 => Ok(BasicColor::LightYellow),
+			94 => Ok(BasicColor::LightBlue),
+			95 => Ok(BasicColor::LightMagenta),
+			96 => Ok(BasicColor::LightCyan),
+			97 => Ok(BasicColor::White),
+			_ => Err(())
+		}
 	}
 }
